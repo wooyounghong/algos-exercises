ac33fdffac36fc6e8e96160ff01e2f3aa8107434
Wayne Hong <woo.young.hong@hotmail.com>
2018-10-10 14:05:33 -0400

    Tree data stucture completed

--- additional-sources/tree/tree.js
+++ additional-sources/tree/tree.js
@@ -9,16 +9,32 @@ Your tree should have methods named "addChild" and "contains".
 // tree.contains(2);   // yields 'true'
 
 var treeMaker = function(value) {
-  //tree code goes here!
+  var newTree = Object.create(treeMaker.methods);
+  newTree.value = value;
+  newTree.children = [];
+  return newTree;
 };
 
 //methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(
-) {
+treeMaker.methods.addChild = function(value) {
+  var node = treeMaker(value);
+  // console.log('this', this);
+  this.children.push(node);
 };
 
-treeMaker.methods.contains = function(
-) {
+treeMaker.methods.contains = function(value) {
+  // check if the root node contains value;
+  if (this.value === value) return true;
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(value)) return true;
+  }
+  return false;
 };
+
+var Tree = treeMaker(1);
+Tree.addChild(2);
+Tree.addChild(3);
+console.log(Tree.contains(4));
+// console.log('newTree', Tree);
